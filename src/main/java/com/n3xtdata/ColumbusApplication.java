0d93e13e9d04e5e199d5b7f2272ca8752daf4a98
Robert Püschel <robert.pueschel@me.com>
2018-09-14 09:43:57 +0200

    added gitignore

---
+++ src/main/java/com/n3xtdata/ColumbusApplication.java
@@ -0,0 +1,16 @@
+package com.n3xtdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class ColumbusApplication {
+
+
+  public static void main(String[] args) {
+    SpringApplication.run(ColumbusApplication.class, args);
+  }
+
+}
